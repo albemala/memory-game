be3d35d39cf4402b52003b7e7cc2519c6961382b
Unknown <albemala@gmail.com>
2020-05-08 10:53:15 +0100

    initial app setup

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const Title = () => <h1>Welcome to <span>Memory Game</span></h1>;
+
+const GameBoard = () => <div/>;
+
+const App = () => (
+    <div>
+        <Title/>
+        <GameBoard/>
+    </div>
+);
+
+export default App;
